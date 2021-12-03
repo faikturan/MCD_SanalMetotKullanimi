ad65ee6285797cb8f2ea691372e2a91bf033a49b
Faik Turan <turanfaik@hotmail.com>
2021-12-03 17:16:53 +0300

    code changed

--- MCD_SanalMetotKullanimi/Program.cs
+++ MCD_SanalMetotKullanimi/Program.cs
@@ -10,6 +10,18 @@ namespace MCD_SanalMetotKullanimi
     {
         static void Main(string[] args)
         {
+            televizyon T1 = new televizyon();
+            T1.ekranaYaz("Hangisi ? ");
+
+
+            Console.WriteLine("************");
+            urun U1 = new urun();
+            U1.ekranaYaz("Merhaba");
+            Console.WriteLine("************");
+            baseClass B1 = new baseClass();
+            B1.ekranaYaz("Merhaba");
+
+            Console.ReadKey();
         }
     }
 }
