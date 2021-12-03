e063c8be1f55bed31a2f1454c1a3fe489f19e051
Faik Turan <turanfaik@hotmail.com>
2021-12-03 17:05:13 +0300

    Add project files.

---
+++ MCD_SanalMetotKullanimi/urun.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_SanalMetotKullanimi
+{
+    public class urun:baseClass
+    {
+        public urun()
+        {
+            Console.WriteLine("urun");
+        }
+
+        public override void ekranaYaz(string data)
+        {
+            Console.WriteLine("urun => " +data);
+        }
+    }
+}
